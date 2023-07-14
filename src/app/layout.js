03519a0cb902431e57933f0edd47293d6cf0f920
James Titus <titusjames299@gmail.com>
2023-07-14 20:30:18 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,7 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'James Titus | Web Dev',
+  title: 'James Titus',
   description: 'James Titus Portfolio',
 }
 
